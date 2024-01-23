7d4c1bb880bce9095bb07d07375e62a6f0a4a5e9
Idot360 <evan.h@outlook.com>
2024-01-23 10:33:34 +0800

    Update tray_match.py
    Added output formatting and certainty

--- tray_match.py
+++ tray_match.py
@@ -6,16 +6,18 @@ import pandas as pd
 
 # Constants
 
+CSV_FILE = "Lettertray_Combined.csv"
 NAME_W = 0.8
 GENDER_W = 0.6
 SUBJECT_W = 0.5
 FULL_W = 0.8
 PARTIAL_W = 0.4
-M_BREAKPOINT = 1
-H_BREAKPOINT = 2
+M_BREAKPOINT = 0.8
+H_BREAKPOINT = 1.5
+DIFF_BREAKPOINT = 0.3
 SUBJECT_SHORT = {"la": "Language Arts", "math": "Mathematics", "cl": "Chinese", "ml": "Malay", "mi": "Man & ideas", 
                  "bio": "Science", "biology": "Science", "phy": "Science", "physics": "Science", "chem": "Science", 
-                 "chemistry": "Science", "com": "Computing"}
+                 "chemistry": "Science", "com": "Computing", "english": "Language Arts"}
 GENDER_SHORT = {"m": "Male", "f": "Female"}
 
 
@@ -35,8 +37,8 @@ def compare(field: str, keywords: str, issubject: bool = False):
     partial_match = False
     #print(keywords, field)
     for word in keywords.split(" "):
-        partial_match = partial_match or word.casefold() in field.casefold()
-    weight += PARTIAL_W*int(partial_match)
+        partial_match = word.casefold() in field.casefold()
+        weight += PARTIAL_W*int(partial_match)
     weight += FULL_W*int(keywords.casefold() == field.casefold() and not issubject)
     return weight
 
@@ -82,7 +84,7 @@ def main():
     given_name = input("please input the name of the teacher: ")
     given_gender = input("please input the gender of the teacher: ")
     given_subject = input("please input the subject/department the teacher teaches: ")
-    tray_database = pd.read_csv("Lettertray_Combined.csv")
+    tray_database = pd.read_csv(CSV_FILE)
     names = tray_database["Full Name"].fillna("Unknown")
     genders = tray_database["Gender"].fillna("Unknown")
     subjects = tray_database["Subject"].fillna("Unknown")
@@ -93,13 +95,36 @@ def main():
     tray_database["Weight"] = weights
     sorted_data = (tray_database.sort_values(by="Weight", ascending=False))
 
-    print(f"""You searched for Mr/Ms/Mrs/Mdm/Dr {given_name}, of gender {given_gender}, who teaches the {given_subject} subject.
+    certainty = ""
+    if sorted_data.Weight[0] > H_BREAKPOINT:
+        certainty = "high"
+    elif sorted_data.Weight[1] > M_BREAKPOINT:
+        certainty = "medium"
+    else:
+        certainty = "low"
+
+    print(f"You searched for Mr/Ms/Mrs/Mdm/Dr {given_name}, of gender {given_gender}, who teaches the {given_subject} subject.")
+    
+    if sorted_data.Weight.iloc[0] == 0:
+        print("No Results\n")
+        return
+    
+    list_value = 0
+    for i in range(5):
+        if sorted_data.Weight.iloc[i] == 0:
+            break
+        list_value += 1
+        
+    print(f"""Likely teachers and locations, with a {certainty} degree of certainty:\n{sorted_data.iloc[:list_value]}""")
+    
+    if sorted_data.Weight.iloc[0] - sorted_data.Weight.iloc[1] >= DIFF_BREAKPOINT:
+        print(f"""
 Most likely location: 
-    {sorted_data.Location[0]}
+    {sorted_data.Location.iloc[0]}
     column {sorted_data.Coordinates.iloc[0].split(',')[0]}  row {sorted_data.Coordinates.iloc[0].split(',')[1]}.
-Likely teachers and locations: \n{sorted_data.iloc[:5]}
 """)
     
+
             # NOTE: CHANGE THE CODE TO SHOW DEGREE OF CERTAINTY;
             #       ONLY PROVIDE MOST LIKELY LOCATION IN CASE OF LARGE DIFFERENCE IN WEIGHT BETWEEN POSITION 1 AND 2
             #       
